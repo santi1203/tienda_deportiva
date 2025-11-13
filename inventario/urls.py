4e156987021c7d27eaa22584fd61abb2bc153977
santiagoo1203 <131529290+santi1203@users.noreply.github.com>
2025-11-13 18:55:31 -0500

    Actualización lista genérica

--- inventario/urls.py
+++ inventario/urls.py
@@ -9,11 +9,11 @@ urlpatterns = [
     path('producto/crear/', views.crear_producto, name='crear_producto'),
     path('producto/<int:producto_id>/editar/', views.editar_producto, name='editar_producto'),
     path('', views.inicio, name='inicio'),
-    path('productos/', views.lista_productos, name='lista_productos'),
+    #path('productos/', views.lista_productos, name='lista_productos'),
     path('producto/<int:producto_id>/eliminar/', views.eliminar_producto, name='eliminar_producto'),
     path('categoria/crear/ajax/', views.crear_categoria_ajax, name='crear_categoria_ajax'),
     path('categoria/crear/', views.crear_categoria, name='crear_categoria'),
 
     #Vista genérica
-    path('productos-generica/', ProductoListView.as_view(), name='productos_generica'),
+    path('productos/', ProductoListView.as_view(), name='productos_generica'),
 ]
