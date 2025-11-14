7a75d05a6284f7bccaf6414b2a0a4ca4b836e911
santiagoo1203 <131529290+santi1203@users.noreply.github.com>
2025-11-13 19:04:57 -0500

    Actualización urls.py

--- inventario/urls.py
+++ inventario/urls.py
@@ -9,7 +9,6 @@ urlpatterns = [
     path('producto/crear/', views.crear_producto, name='crear_producto'),
     path('producto/<int:producto_id>/editar/', views.editar_producto, name='editar_producto'),
     path('', views.inicio, name='inicio'),
-    #path('productos/', views.lista_productos, name='lista_productos'),
     path('producto/<int:producto_id>/eliminar/', views.eliminar_producto, name='eliminar_producto'),
     path('categoria/crear/ajax/', views.crear_categoria_ajax, name='crear_categoria_ajax'),
     path('categoria/crear/', views.crear_categoria, name='crear_categoria'),
