287e4b1bcf1eca08c04f2aa24b12e805cefaedcd
santiagoo1203 <131529290+santi1203@users.noreply.github.com>
2025-11-13 19:05:45 -0500

    Actualización urls.py

--- inventario/urls.py
+++ inventario/urls.py
@@ -4,7 +4,6 @@ from .views import ProductoListView
 
 urlpatterns = [
     path('', views.inicio, name='inicio'),
-    path('productos/', views.lista_productos, name='lista_productos'),
     path('producto/<int:producto_id>/', views.detalle_producto, name='detalle_producto'),
     path('producto/crear/', views.crear_producto, name='crear_producto'),
     path('producto/<int:producto_id>/editar/', views.editar_producto, name='editar_producto'),
